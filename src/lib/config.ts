a57630390dcffc7c7ca1140c7cac85c720146f63
Muhammad Abdul Hafidz <hfidz7534@gmail.com>
2023-10-11 00:35:08 +0700

    a

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from "$app/environment"
+
+export const title = 'Hafidz'
+export const description = 'Svelte Kit Cashier'
+export const url = dev ? 'http://localhost:5173/' : 'http://fidzcode:xyz/'
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Coffee Cashier</h1>
+<p>Visit to read the documentation</p>
