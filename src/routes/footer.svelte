1b7f06f09173199197259c3394eac6f35e1d552c
Muhammad Abdul Hafidz <hfidz7534@gmail.com>
2023-11-05 11:21:46 +0700

    feat(footer)

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -126,14 +126,14 @@
     </div>
     <hr class="my-6 border-blueGray-300" />
     <div class="flex flex-wrap items-center md:justify-between justify-center">
-        <div class="w-full md:w-4/12 px-4 mx-auto text-center">
+        <div class="w-full md:w-6/12 px-4 mx-auto text-center">
         <div class="text-sm text-blueGray-500 font-semibold py-1">
-            Copyright © {date} Notus Svelte by
+            Copyright © {date} Coffee Cashier App by
             <a
             href="https://www.creative-tim.com?ref=ns-footer"
             class="text-blueGray-500 hover:text-blueGray-800"
             >
-            Creative Tim
+            M Abdul Hafidz
             </a>
             .
         </div>
